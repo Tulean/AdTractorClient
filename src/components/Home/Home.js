66d2ef4e6c0d53af661bb773e66c1d84e520d7a6
kevin4coding <jiahao.liu@sjsu.edu>
2019-04-24 13:02:47 -0700

    UI roughly done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -35,7 +35,7 @@ const styles = theme => ({
       height: 'calc(100vh - 60px)'
     }
   },
-  slide2:{
+  slide4:{
     background:
       'linear-gradient(to right, rgba(255, 255, 255, 0.8) 10%, rgba(255, 255, 255, 0.8) 40%, rgba(0, 0, 0, 0.4)), url("/pictures/ethereum.jpg")',
     width: '100%',
@@ -61,7 +61,7 @@ const styles = theme => ({
       height: 'calc(100vh - 60px)'
     }
   },
-  slide4:{
+  slide2:{
     background:
       'linear-gradient(to right, rgba(255, 255, 255, 0.8) 10%, rgba(255, 255, 255, 0.8) 40%, rgba(0, 0, 0, 0.4)), url("/pictures/dAPP.jpg")',
     width: '100%',
@@ -128,6 +128,18 @@ export class Home extends Component {
     this.setState({ open: true });
   };
 
+  handleOpenOutSide = () => {
+    window.open('https://www.serpwizard.com/differences-between-web-3-0-and-web-2-0-websites/', '_blank')
+  };
+
+  handleOpenMetamask = () => {
+    window.open('https://metamask.io/', '_blank')
+  };
+
+  handleOpenEthereum = () => {
+    window.open('https://www.ethereum.org/', '_blank')
+  }
+
   handleClose = () => {
     this.setState({ open: false });
   };
@@ -152,12 +164,12 @@ export class Home extends Component {
       <div className={classes.root}>
       <Tabs value={index} fullWidth onChange={this.handleChange} style={styles.tabs}>
         <Tab label="homepage" />
-        <Tab label="ethereum" />
-        <Tab label="metamask" />
         <Tab label="dAPP" />
+        <Tab label="metamask" />
+        <Tab label="Ethereum" />
       </Tabs>
       <AutoPlaySwipeableViews index={index} onChangeIndex={this.handleChangeIndex}>
-      <div className={classes.slide1} label="homepage">
+      <div className={classes.slide1}>
         <Grid container spacing={24}>
             <Grid
               className={classes.textHome}
@@ -221,7 +233,8 @@ export class Home extends Component {
             <GetStarted />
           </Modal>
       </div>
-      <div className={classes.slide2}><Grid container spacing={24}>
+      <div className={classes.slide2}>
+        <Grid container spacing={24}>
           <Grid
             className={classes.textHome}
             container
@@ -230,45 +243,58 @@ export class Home extends Component {
             direction="column"
           >
             <Grid container item direction="row" className={classes.title}>
-              Welcome to AdTractor
-            </Grid>
-            <Grid
-              continaer
-              item
-              direction="row"
-              className={classes.secondaryText}
-            >
-              <Grid container item direction="row" spacing={24}>
-                <Grid item xs={12} className={classes.secondaryText}>
-                  A Decentralized Advertising Network
-                </Grid>
-                <Grid item xs={12} align="center">
-                  <Button
-                    variant="outlined"
-                    className={classes.learnButton}
-                    onClick={this.handleOpen}
-                  >
-                    Get Started
-                  </Button>
-                </Grid>
-              </Grid>
+              What is Web3
             </Grid>
             <Grid container item direction="row" className={classes.text}>
               <Grid item xs={5} className={classes.text}>
                 <Typography paragraph="true" variant="subtitle1">
-                  Are you looking for a secure advertising network? Look no
-                  further, AdTractor prevents fraud by utilizing security
-                  features provided by the Ethereum blockchain. Our advertising
-                  network has the potential to save companies billions of
-                  dollars per year. Advertise with us before its too late.
+                  Are you still using and developping web2? Sadly, it will 
+                  become the past tense soon. Why? Web 2.0 is a writable  
+                  and more social oriented World Wide Web that uses Google 
+                  as a catalyst to perform. It means we have a CENTRALIZED
+                  server -- Google. Everyone's data is stored in here and 
+                  sellable. Web3 is a dAPP -- Decentralized Application. 
                 </Typography>
                 <Typography variant="button">
                   <Button
                     variant="outlined"
                     className={classes.learnButton}
-                    onClick={this.handleOpen}
+                    onClick={this.handleOpenOutSide}
                   >
-                    Get Started
+                    Learn More
+                  </Button>
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </div>
+      <div className={classes.slide3}>
+        <Grid container spacing={24}>
+          <Grid
+            className={classes.textHome}
+            container
+            item
+            xs={12}
+            direction="column"
+          >
+            <Grid container item direction="row" className={classes.title}>
+              Metamask now!
+            </Grid>
+            <Grid container item direction="row" className={classes.text}>
+              <Grid item xs={5} className={classes.text}>
+                <Typography paragraph="true" variant="subtitle1">
+                MetaMask is a bridge that allows you to visit the distributed web of tomorrow 
+                in your browser today. It allows you to run Ethereum dApps right in your browser 
+                without running a full Ethereum node
+                </Typography>
+                <Typography variant="button">
+                  <Button
+                    variant="outlined"
+                    className={classes.learnButton}
+                    onClick={this.handleOpenMetamask}
+                  >
+                    Install Metamask 
                   </Button>
                 </Typography>
               </Grid>
@@ -284,7 +310,8 @@ export class Home extends Component {
           <GetStarted />
         </Modal>
       </div>
-      <div className={classes.slide3}><Grid container spacing={24}>
+      <div className={classes.slide4}>
+        <Grid container spacing={24}>
           <Grid
             className={classes.textHome}
             container
@@ -293,122 +320,30 @@ export class Home extends Component {
             direction="column"
           >
             <Grid container item direction="row" className={classes.title}>
-              Welcome to AdTractor
-            </Grid>
-            <Grid
-              continaer
-              item
-              direction="row"
-              className={classes.secondaryText}
-            >
-              <Grid container item direction="row" spacing={24}>
-                <Grid item xs={12} className={classes.secondaryText}>
-                  A Decentralized Advertising Network
-                </Grid>
-                <Grid item xs={12} align="center">
-                  <Button
-                    variant="outlined"
-                    className={classes.learnButton}
-                    onClick={this.handleOpen}
-                  >
-                    Get Started
-                  </Button>
-                </Grid>
-              </Grid>
+              New money - Ethereum
             </Grid>
             <Grid container item direction="row" className={classes.text}>
               <Grid item xs={5} className={classes.text}>
                 <Typography paragraph="true" variant="subtitle1">
-                  Are you looking for a secure advertising network? Look no
-                  further, AdTractor prevents fraud by utilizing security
-                  features provided by the Ethereum blockchain. Our advertising
-                  network has the potential to save companies billions of
-                  dollars per year. Advertise with us before its too late.
+                Ethereum is an open-source, public, blockchain-based distributed 
+                computing platform and operating system featuring smart contract 
+                functionality. It supports a modified version of Nakamoto consensus 
+                via transaction-based state transitions. Ether is a token whose 
+                blockchain is generated by the Ethereum platform
                 </Typography>
                 <Typography variant="button">
                   <Button
                     variant="outlined"
                     className={classes.learnButton}
-                    onClick={this.handleOpen}
+                    onClick={this.handleOpenEthereum}
                   >
-                    Get Started
+                    Learn More
                   </Button>
                 </Typography>
               </Grid>
             </Grid>
           </Grid>
         </Grid>
-        <Modal
-          aria-labelledby="simple-modal-title"
-          aria-describedby="simple-modal-description"
-          open={this.state.open}
-          onClose={this.handleClose}
-        >
-          <GetStarted />
-        </Modal>
-      </div>
-      <div className={classes.slide4}><Grid container spacing={24}>
-          <Grid
-            className={classes.textHome}
-            container
-            item
-            xs={12}
-            direction="column"
-          >
-            <Grid container item direction="row" className={classes.title}>
-              Welcome to AdTractor
-            </Grid>
-            <Grid
-              continaer
-              item
-              direction="row"
-              className={classes.secondaryText}
-            >
-              <Grid container item direction="row" spacing={24}>
-                <Grid item xs={12} className={classes.secondaryText}>
-                  A Decentralized Advertising Network
-                </Grid>
-                <Grid item xs={12} align="center">
-                  <Button
-                    variant="outlined"
-                    className={classes.learnButton}
-                    onClick={this.handleOpen}
-                  >
-                    Get Started
-                  </Button>
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid container item direction="row" className={classes.text}>
-              <Grid item xs={5} className={classes.text}>
-                <Typography paragraph="true" variant="subtitle1">
-                  Are you looking for a secure advertising network? Look no
-                  further, AdTractor prevents fraud by utilizing security
-                  features provided by the Ethereum blockchain. Our advertising
-                  network has the potential to save companies billions of
-                  dollars per year. Advertise with us before its too late.
-                </Typography>
-                <Typography variant="button">
-                  <Button
-                    variant="outlined"
-                    className={classes.learnButton}
-                    onClick={this.handleOpen}
-                  >
-                    Get Started
-                  </Button>
-                </Typography>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
-        <Modal
-          aria-labelledby="simple-modal-title"
-          aria-describedby="simple-modal-description"
-          open={this.state.open}
-          onClose={this.handleClose}
-        >
-          <GetStarted />
-        </Modal>
       </div>
       </AutoPlaySwipeableViews>
       </div>
